c8825dabec4e6f51b6474b482da70ed7558074a7
matt <matt@83f10663-3a1f-dc11-aaad-0014220fd315>
2007-07-02 11:32:14 +0000

    Photo upload mechanics
    git-svn-id: file:///Users/matt/Sites/dramasoc/trunk@75 83f10663-3a1f-dc11-aaad-0014220fd315

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,10 @@
 module PhotosHelper
+  def attachable_photos_path(attachable)
+    if attachable.nil?
+      photos_path
+    else
+      method_name = "#{attachable.class.table_name.singularize}_photos_path"
+      send method_name, attachable
+    end
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,17 +1,19 @@
 class Photo < ActiveRecord::Base
-  has_attachment :storage => :file_system, :max_size => 1.megabytes, :thumbnails => { :thumb => '80x80>', :tiny => '40x40>' }, :processor => Photo.processor, :resize_to => '600x600>'
+protected
+  def self.processor
+    begin
+      options = YAML::load_file 'config/site_options.yml'
+      options['photos']['processor']
+    rescue Errno::ENOENT, IndexError
+      :imageScience
+    end
+  end
+  
+public
+  
+  has_attachment :storage => :file_system, :max_size => 1.megabytes, :thumbnails => { :thumb => '80x80>', :tiny => '40x40>' }, :processor => processor, :resize_to => '600x600>'
   
   validates_as_attachment
   
-  class << self
-    def processor
-      begin
-        options = YAML::load_file 'config/site_options.yml'
-        options.with_indifferent_access!
-        options[:photos][:processor]
-      rescue Errno::ENOENT, IndexError
-        :imageScience
-      end
-    end
-  end
+
 end
